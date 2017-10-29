8fb49109cae583fd63ecf9efe53741dfd0388108
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-10-29 17:11:09 -0400

    Add support for a prefix path.

--- nengo_gui/guibackend.py
+++ nengo_gui/guibackend.py
@@ -97,7 +97,8 @@ class RequireAuthentication(object):
                 session.login_host = inst.headers.get('host', None)
                 inst.persist_session(session)
                 return fn(inst)
-            return server.HttpRedirect(self.login_page)
+            return server.HttpRedirect(
+                inst.server.settings.prefix + self.login_page)
         return auth_checked
 
 
@@ -115,12 +116,18 @@ class GuiRequestHandler(server.HttpWsRequestHandler):
         login_host = self.get_session().login_host
         return [login_host] if login_host is not None else []
 
+    def http_GET(self):
+        if not self.resource.startswith(self.server.settings.prefix):
+            raise server.InvalidResource(self.resource)
+        self.resource = self.resource[len(self.server.settings.prefix):]
+        return super(GuiRequestHandler, self).http_GET()
+
     def login_page(self):
         session = self.get_session()
         content = b''
 
         if session.authenticated:
-            return server.HttpRedirect('/')
+            return server.HttpRedirect(self.server.settings.prefix + '/')
 
         if 'pw' in self.db:
             valid_pw = (
@@ -131,7 +138,7 @@ class GuiRequestHandler(server.HttpWsRequestHandler):
                 session.authenticated = True
                 session.login_host = self.headers.get('host', None)
                 self.persist_session(session)
-                return server.HttpRedirect('/')
+                return server.HttpRedirect(self.server.settings.prefix + '/')
             else:
                 content += b'<p><strong>Invalid password. Try again.'
                 content += b'</strong></p>'
@@ -311,6 +318,7 @@ class GuiRequestHandler(server.HttpWsRequestHandler):
     def persist_session(self, session):
         session_id = self.server.sessions.add_session(self.request, session)
         self.cookie['_session_id'] = session_id
+        self.cookie['_session_id']['path'] = self.server.settings.prefix
         self.cookie['_session_id']['httponly'] = True
         self.cookie['_session_id']['max-age'] = (
             self.server.settings.session_duration)
@@ -358,18 +366,20 @@ class GuiServerSettings(object):
         'ssl_cert',
         'ssl_key',
         'session_duration',
+        'prefix'
     ]
 
     def __init__(
             self, listen_addr=('localhost', 8080), auto_shutdown=2,
             password_hash=None, ssl_cert=None, ssl_key=None,
-            session_duration=60 * 60 * 24 * 30):
+            session_duration=60 * 60 * 24 * 30, prefix=''):
         self.listen_addr = listen_addr
         self.auto_shutdown = auto_shutdown
         self.password_hash = password_hash
         self.ssl_cert = ssl_cert
         self.ssl_key = ssl_key
         self.session_duration = session_duration
+        self.prefix = prefix
 
     @property
     def use_ssl(self):
