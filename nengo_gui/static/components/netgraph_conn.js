eb71aaa923e3d694a2ea520df74662df8f133675
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2017-12-06 15:06:03 -0500

    Fix inhibitory marker rotation

--- nengo_gui/static/components/netgraph_conn.js
+++ nengo_gui/static/components/netgraph_conn.js
@@ -95,7 +95,7 @@ Nengo.NetGraphConnection.prototype.create_line = function() {
         if (this.minimap == false) {
             switch (this.kind) {
                 case "inhibitory":
-                    this.marker.setAttribute('d', "M 7,7 C 10.5,3.5 7,0 7,0 L 0,7 c 0,0 3.5,3.5 7,0 z");
+                    this.marker.setAttribute('d', "M 10,5.5 c 0,-5 -5,-5 -5,-5 V 10 c 0,0 5,0 5,-5 z");
                     break;
                 case "modulatory":
                     this.marker.setAttribute('d', "M 7.5,0 0,-5 -7.5,0 0,5 z");
